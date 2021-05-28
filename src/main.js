747cdc2bbfe8b93a9ba87c0bc5641b2c06d00311
wangcho1999 <eastsky21@gmail.com>
2021-05-28 17:45:21 +0900

    firestore test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import firebaseConfig from '../firebaseConfig'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
+
+// Initialize Firebase
+var db = firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+db.firestore().collection("test").add(
+  {
+    test: [1,0]
+  }
+).then(r => console.log(r)).catch(e => console.log(e));
+
